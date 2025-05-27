804c5a97f09631664aece4c3c87d1a957d5d0142
Gaurav <12310132@nitkkr.ac.in>
2025-05-27 20:27:11 +0530

    Added Confidence Threshold for better Detection
    Created a dynamic confidence threshold for better detection. It matches with the data which gives maximum confidence score. Update GUI to set threshold values.

--- Face_Recoganition_OpenCV/run.py
+++ Face_Recoganition_OpenCV/run.py
@@ -1,4 +1,17 @@
 import Interface
+import sys
 
 if __name__ == "__main__":
-    Interface.interFace()
+    confidence_match = 0.4
+    confidence_save = 0.4
+    confidence_show = True
+    if(len(sys.argv) > 1):
+        if(sys.argv[1] and sys.argv[1].lower == 'false'):
+                confidence_show = False
+        if(sys.argv[2]):
+            confidence_match = (100-float(sys.argv[2]))/100
+        if(sys.argv[3]):
+            confidence_save = (100-float(sys.argv[3]))/100
+        elif(sys.argv[2]):
+            confidence_save = (100-float(sys.argv[2]))/100
+    Interface.interFace(confidence_match,confidence_save,confidence_show)
